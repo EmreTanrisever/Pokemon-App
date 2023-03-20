aa9485cc5aa10538e42a74780c443736e3d37001
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-20 19:14:47 +0300

    Create project

---
+++ Pokemon/Pokemon/HomeController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokemon
+//
+//  Created by Emre Tanrısever on 20.03.2023.
+//
+
+import UIKit
+
+class HomeController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .yellow
+    }
+
+
+}
+
