f21d84a78a92fe5dc54b3036274ad07544d953a0
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-22 22:12:05 +0300

    Create pokemon detail model

--- Pokemon/Pokemon/Models/ResponseModel/PokemonDetail.swift
+++ Pokemon/Pokemon/Models/ResponseModel/PokemonDetail.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct PokemonDetail: Codable {
+    let id: Int
+    let height: Int
+    let name: String
+    let order: Int
+    let sprites: Sprites
+    let stats: [Stats]
+    let types: [Types]
+    let weight: Int
+}
