f3b36105e54c052d818344399cbd06b915a9738a
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-22 16:26:10 +0300

    Create home controller and table view

--- Pokemon/Pokemon/Scenes/Home/HomeController.swift
+++ Pokemon/Pokemon/Scenes/Home/HomeController.swift
@@ -7,14 +7,61 @@
 
 import UIKit
 
-class HomeController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        view.backgroundColor = .yellow
-    }
-
-
+protocol HomeControllerInterface: AnyObject {
+    func configure()
+    func prepareTableView()
+    func reloadDataTableView()
 }
 
+final class HomeController: UIViewController, HomeControllerInterface {
+
+    private let pokemonsTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    private lazy var viewModel = HomeViewModel(self)
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        viewModel.viewDidLoad()
+    }
+}
+
+extension HomeController {
+    func configure() {
+        view.addSubview(pokemonsTableView)
+        
+        setConstraints()
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            pokemonsTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            pokemonsTableView.topAnchor.constraint(equalTo: view.topAnchor),
+            pokemonsTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            pokemonsTableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
+}
+
+extension HomeController: UITableViewDelegate, UITableViewDataSource {
+    
+    func prepareTableView() {
+        pokemonsTableView.delegate = self
+        pokemonsTableView.dataSource = self
+    }
+    
+    func reloadDataTableView() {
+        pokemonsTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        UITableViewCell()
+    }
+}
