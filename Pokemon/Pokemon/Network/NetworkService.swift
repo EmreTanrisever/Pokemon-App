d93cec030445c57c51fa703363886af123884dc7
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-22 22:12:30 +0300

    Add fetchDetailOfPokemons functions

--- Pokemon/Pokemon/Network/NetworkService.swift
+++ Pokemon/Pokemon/Network/NetworkService.swift
@@ -10,12 +10,20 @@ import Foundation
 protocol NetworkServiceInterface {
     
     func fetchPokemons(completion: @escaping(Result<PokemonsResponse, NetworkError>) -> Void)
+    func fetchDetailOfPokemons(name: String, completion: @escaping(Result<PokemonDetail, NetworkError>) -> Void)
 }
 
 final class NetworkService: NetworkServiceInterface {
     
     func fetchPokemons(completion: @escaping(Result<PokemonsResponse, NetworkError>) -> Void) {
         guard let url = PokemonsRequestModel.shared.createURL(with: nil) else { return }
+        NetworkManager.shared.sendRequest(urlRequest: url) { response in
+            completion(response)
+        }
+    }
+    
+    func fetchDetailOfPokemons(name: String, completion: @escaping(Result<PokemonDetail, NetworkError>) -> Void) {
+        guard let url = PokemonsDetailRequestModel.shared.createURL(with: name) else { return }
         NetworkManager.shared.sendRequest(urlRequest: url) { response in
             completion(response)
         }
