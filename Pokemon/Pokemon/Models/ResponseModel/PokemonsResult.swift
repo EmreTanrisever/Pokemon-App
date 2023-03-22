f26bfccde651689dd8948af9600739f3073bc1a2
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-22 17:20:39 +0300

    Create pokemons response models

---
+++ Pokemon/Pokemon/Models/ResponseModel/PokemonsResult.swift
@@ -0,0 +1,13 @@
+//
+//  PokemonsResult.swift
+//  Pokemon
+//
+//  Created by Emre Tanrısever on 22.03.2023.
+//
+
+import Foundation
+
+struct PokemonResult: Codable {
+    let name: String
+    let url: String
+}
---
+++ Pokemon/Pokemon/Utilities/Enums/NetworkErrorEnum.swift
@@ -0,0 +1,8 @@
+//
+//  NetworkEnum.swift
+//  Pokemon
+//
+//  Created by Emre Tanrısever on 22.03.2023.
+//
+
+import Foundation
