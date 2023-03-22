49a2b2f88c5e5b3bd43a53f9d9d74a89b5e4f98e
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-22 22:11:22 +0300

    Create pokemons detail request model

--- Pokemon/Pokemon/Models/RequestModel/PokemonsDetailRequestModel.swift
+++ Pokemon/Pokemon/Models/RequestModel/PokemonsDetailRequestModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class PokemonsDetailRequestModel: BaseRequestModel {
+    
+    static let shared = PokemonsDetailRequestModel()
+    
+    override var schema: String {
+        Constants.NetworkConstant.pokemonSchema
+    }
+    
+    override var host: String {
+        Constants.NetworkConstant.pokemonHost
+    }
+    
+    override var path: String {
+        Constants.NetworkConstant.pokemonPath
+    }
+}
