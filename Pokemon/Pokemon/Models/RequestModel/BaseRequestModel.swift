6ec2d8870175dc820504921240aa50a20f4efa85
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-22 15:55:48 +0300

    Create base request model

--- Pokemon/Pokemon/Models/RequestModel/BaseRequestModel.swift
+++ Pokemon/Pokemon/Models/RequestModel/BaseRequestModel.swift
@@ -6,3 +6,59 @@
 //
 
 import Foundation
+
+protocol BaseRequestModelInterface {
+    var schema: String { get }
+    var host: String { get }
+    var path: String { get }
+    var queryItems: [String: Any] { get }
+}
+
+class BaseRequestModel: BaseRequestModelInterface {
+
+    var schema: String {
+            "https"
+        }
+        
+        var host: String {
+            "api.themoviedb.org"
+        }
+        
+        var path: String {
+            ""
+        }
+        
+        var queryItems: [String : Any] {
+            [:]
+        }
+    
+    func createURL(with pokemonID: Int?) -> URLRequest? {
+        var component = URLComponents()
+        component.scheme = schema
+        component.host = host
+        var items: [URLQueryItem] = []
+        
+        var pathWithID = ""
+        
+        if let id = pokemonID {
+            pathWithID = "\(path)/\(id)"
+            component.path = pathWithID
+        } else {
+            component.path = path
+            
+        }
+        
+        for (key, value) in queryItems {
+            let queryItem = URLQueryItem(name: key, value: "\(value)")
+            items.append(queryItem)
+        }
+
+        component.queryItems = items
+        
+        if let url = component.url {
+            return URLRequest(url: url)
+        } else {
+            return nil
+        }
+    }
+}
