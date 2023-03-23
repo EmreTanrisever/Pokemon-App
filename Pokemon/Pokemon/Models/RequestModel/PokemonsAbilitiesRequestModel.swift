11ba206b0c2c9525736b225f0a86c5e374d5c974
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-23 21:07:32 +0300

    Create pokemon ability response model

--- Pokemon/Pokemon/Models/RequestModel/PokemonsAbilitiesRequestModel.swift
+++ Pokemon/Pokemon/Models/RequestModel/PokemonsAbilitiesRequestModel.swift
@@ -6,3 +6,20 @@
 //
 
 import Foundation
+
+final class PokemonsAbilitiesRequestModel: BaseRequestModel {
+    
+    static let shared = PokemonsAbilitiesRequestModel()
+    
+    override var schema: String {
+        Constants.NetworkConstant.pokemonSchema
+    }
+    
+    override var host: String {
+        Constants.NetworkConstant.pokemonHost
+    }
+    
+    override var path: String {
+        Constants.NetworkConstant.abilityPath
+    }
+}
