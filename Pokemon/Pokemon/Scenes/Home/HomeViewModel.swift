c563f37505bbeb1e59feccc14865b5b5cfa4f81d
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-23 21:10:11 +0300

    Fetch pokemons and change some lines

--- Pokemon/Pokemon/Scenes/Home/HomeViewModel.swift
+++ Pokemon/Pokemon/Scenes/Home/HomeViewModel.swift
@@ -9,13 +9,23 @@ import Foundation
 import UIKit
 
 protocol HomeViewModelInterface {
-    var view: HomeControllerInterface? { get }
+    
+    var view: HomeControllerInterface? { get set}
+    var pokemons: [PokemonsResponse] { get set }
+    var pokemonsDetail: [PokemonDetail] { get set }
     
     func viewDidLoad()
+    func getPokemons()
+    func getDetailOfPokemon(name: String)
 }
 
 final class HomeViewModel {
+    
     weak var view: HomeControllerInterface?
+    private let service = NetworkService()
+    
+    var pokemons: [PokemonsResponse] = []
+    var pokemonsDetail: [PokemonDetail] = []
     
     init(_ view: HomeControllerInterface) {
         self.view = view
@@ -27,5 +37,34 @@ extension HomeViewModel: HomeViewModelInterface {
     func viewDidLoad() {
         view?.configure()
         view?.prepareTableView()
+        getPokemons()
     }
+    
+    func getPokemons() {
+        service.fetchPokemons { response in
+            switch response {
+            case .success(let pokemons):
+                self.pokemons.append(pokemons)
+                for item in pokemons.results {
+                    self.getDetailOfPokemon(name: item.name)
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    func getDetailOfPokemon(name: String) {
+        service.fetchDetailOfPokemons(name: name) { response in
+            switch response {
+            case .success(let pokemonDetail):
+                self.pokemonsDetail.append(pokemonDetail)
+                self.view?.reloadDataTableView()
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+ 
 }
