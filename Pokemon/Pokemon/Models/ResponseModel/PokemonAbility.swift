14f10c5bc73625d17782662a956342ee8f361685
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-23 21:07:53 +0300

    Create pokemon response model

--- Pokemon/Pokemon/Models/ResponseModel/PokemonAbility.swift
+++ Pokemon/Pokemon/Models/ResponseModel/PokemonAbility.swift
@@ -6,3 +6,17 @@
 //
 
 import Foundation
+
+struct PokemonAbility: Codable {
+    let effect_entries: [PokemonEffect]
+}
+
+struct PokemonEffect: Codable {
+    let effect: String
+    let language: PokemonLanguage
+    let short_effect: String
+}
+
+struct PokemonLanguage: Codable {
+    let name: String
+}
