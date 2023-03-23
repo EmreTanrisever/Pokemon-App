07ac8f606777a0441fbd819780ae36f2c388b567
EmreTanrisever <emretanrisever.34@hotmail.com>
2023-03-23 21:13:25 +0300

    Create detail view model end finish

--- Pokemon/Pokemon/Scenes/Detail/DetailViewModel.swift
+++ Pokemon/Pokemon/Scenes/Detail/DetailViewModel.swift
@@ -6,3 +6,54 @@
 //
 
 import Foundation
+
+protocol DetailViewModelInterface {
+    var view: DetailViewControllerInterface? { get set }
+    var pokemonAbility: PokemonEffect? { get set }
+    var pokemon: PokemonDetail? { get set }
+    
+    func viewDidLoad()
+    func returnTypeCount(type: [Types])
+    func getAbility(id: Int)
+}
+
+final class DetailViewModel {
+    
+    var view: DetailViewControllerInterface?
+    private var service = NetworkService()
+    var pokemonAbility: PokemonEffect?
+    var pokemon: PokemonDetail?
+    
+    init(_ view: DetailViewControllerInterface) {
+        self.view = view
+    }
+}
+
+extension DetailViewModel: DetailViewModelInterface {
+    
+    func viewDidLoad() {
+        view?.configure()
+    }
+    
+    func returnTypeCount(type: [Types]) {
+        if type.count > 1 {
+            view?.createTypesLabel(types: [type[0].type, type[1].type])
+        } else {
+            view?.createTypeLabel(type: type[0].type)
+        }
+    }
+    
+    func getAbility(id: Int) {
+        service.fetchAbilityOfPokemon(id: id) { response in
+            switch response {
+            case .success(let ability):
+                DispatchQueue.main.async {
+                    self.pokemonAbility = ability.effect_entries[1]
+                    self.view?.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+}
